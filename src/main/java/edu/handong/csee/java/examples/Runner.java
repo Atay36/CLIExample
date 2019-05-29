27c38e34cca88a4f3f7b8bfe16fd937dc08a0b84
YeeunLee <prayatay36@gmail.com>
2019-05-29 16:59:50 +0900

    .

--- src/main/java/edu/handong/csee/java/examples/Runner.java
+++ src/main/java/edu/handong/csee/java/examples/Runner.java
@@ -7,9 +7,13 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Runner {
 	
 	String path;
+	String fullpath;
 	boolean verbose;
 	boolean help;
 
@@ -32,8 +36,19 @@ public class Runner {
 			// path is required (necessary) data so no need to have a branch.
 			System.out.println("You provided \"" + path + "\" as the value of the option p");
 			
+			File file = new File(path);
+
 			// TODO show the number of files in the path
-			
+			String files[] = file.list();
+			int i=0;
+			for(String number : files){
+				//System.out.println(number);
+				i++;
+			}
+			System.out.println(i+" files here");
+
+
+			//System.out.println("files.length : " + files.length);			
 			if(verbose) {
 				
 				// TODO list all files in the path
@@ -53,6 +68,7 @@ public class Runner {
 			path = cmd.getOptionValue("p");
 			verbose = cmd.hasOption("v");
 			help = cmd.hasOption("h");
+			fullpath = cmd.getOptionValue("f");
 
 		} catch (Exception e) {
 			printHelp(options);
@@ -73,8 +89,23 @@ public class Runner {
 				.argName("Path name to display")
 				.required()
 				.build());
+		
+		// add options by using OptionBuilder
+//		options.addOption(Option.builder("f").longOpt("fullpath")
+//				.desc("Set a fullpath of a directory or a file to display")
+//				.hasArg()
+//				.argName("fullpath name to display")
+//				.required()
+//				.build());
 
 		// add options by using OptionBuilder
+		options.addOption(Option.builder("f").longOpt("fullpath")
+				.desc("Display detailed messages!")
+				//.hasArg()     // this option is intended not to have an option value but just an option
+				.argName("fullpath option")
+				//.required() // this is an optional option. So disabled required().
+				.build());	// add options by using OptionBuilder
+		
 		options.addOption(Option.builder("v").longOpt("verbose")
 				.desc("Display detailed messages!")
 				//.hasArg()     // this option is intended not to have an option value but just an option
